e4653601d375524797331bf09b5527834db25f60
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-07-24 13:10:34 +0200

    Remove rsyslog forwarders (#22)

--- lib/utils.py
+++ lib/utils.py
@@ -39,28 +39,3 @@ def update_logrotate_conf(path, frequency=None, retention=0, dateext=True):
             new.append(line)
 
     return '\n'.join(new)
-
-
-def update_rsyslog_default_conf(path):
-    """Updating existing rsyslog 50-default.conf to work around LP:581360."""
-
-    if not os.path.exists(path):
-        return ''
-
-    with open(path, 'r', encoding='utf-8') as f:
-        config = f.read().split('\n')
-
-    new = []
-    regex = re.compile(r'^(\*\.\*;auth,authpriv\.none)(\s+.*)')
-    for line in config:
-        m = regex.match(line)
-        if not m:
-            new.append(line)
-            continue
-
-        conf = m.group(1)
-        dest = m.group(2)
-
-        new.append('{},mail.none,kern.none{}'.format(conf, dest))
-
-    return '\n'.join(new)
