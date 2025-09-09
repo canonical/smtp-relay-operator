2c94edd2e8c7713eb6e9825e5793958a3121a598
Javier de la Puente <javier.delapuente@canonical.com>
2025-09-09 17:02:16 +0200

    change order of calls to give more time for the host change

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -35,12 +35,6 @@ def test_simple_relay(juju: jubilant.Juju, smtp_relay_app, machine_ip_address):
     act: Send an email to an address with the testrelay.internal domain.
     assert: The email is correctly relayed to the mailcatcher local test smtp server.
     """
-    mailcatcher_url = "http://127.0.0.1:1080/messages"
-    messages = requests.get(mailcatcher_url, timeout=5).json()
-    # There should not be any message in mailcatcher before the test.
-    assert len(messages) == 0
-    status = juju.status()
-
     unit = list(status.apps[smtp_relay_app].units.values())[0]
     unit_ip = unit.public_address
 
@@ -48,6 +42,12 @@ def test_simple_relay(juju: jubilant.Juju, smtp_relay_app, machine_ip_address):
         f"echo {machine_ip_address} testrelay.internal | sudo tee -a /etc/hosts"
     )
     juju.exec(machine=unit.machine, command=command_to_put_domain)
+
+    mailcatcher_url = "http://127.0.0.1:1080/messages"
+    messages = requests.get(mailcatcher_url, timeout=5).json()
+    # There should not be any message in mailcatcher before the test.
+    assert len(messages) == 0
+    status = juju.status()
 
     with smtplib.SMTP(unit_ip) as server:
         server.set_debuglevel(2)
